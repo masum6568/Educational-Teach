35f029c1257fcf2fa23e7f4bd9ad3cab5ed0d19b
masum6568 <xyzmasum1134@gmail.com>
2022-03-29 17:25:55 +0600

    second-commit

--- src/components/Extrapart/Extrapart.js
+++ src/components/Extrapart/Extrapart.js
@@ -4,29 +4,24 @@ import '../Extrapart/Extrapart.css'
 
 const Extrapart = () => {
     return (
-        <div className='container'>
+        <div className='container section'>
             <img className='img' src={img4} alt="" />
 
+            <div className='text'>
 
-            <h2>This is extra part
-            </h2>
-            <div className="blue-bg"></div>
-            <div className="white-bg shadow"></div>
-            <div className="content">
-                <h1>Slanted & Enchanted</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin magna ex, elementum id convallis eu, elementum id justo. Maecenas dictum sagittis leo, ut ultrices risus mollis vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                    eu lectus felis. Vivamus eget velit erat. Vivamus condimentum augue sed lacus viverra imperdiet. Etiam quis lectus non libero hendrerit porta. In sit amet molestie lacus. Praesent ultricies, lectus eget porta porttitor, sem sapien facilisis arcu,
-                    eu tincidunt nulla odio ac ipsu.</p>
+                <div className="blue-bg"></div>
+                <div className="white-bg shadow"></div>
+                <div className="content">
+                    <h1>Slanted & Enchanted</h1>
+                    <p>QUT also offers graduate certificates and short courses. Select from these specialisations or build your own course with units of your choice. Master of Education - Choice of Units. Master of Education - Early Childhood Specialisation. Master of Education - Educational Leadership Specialisation.</p>
 
-                <p>Praesent eu arcu convallis, faucibus turpis eu, rutrum nisi. Integer rutrum ipsum at aliquam consequat. In dignissim lorem nibh, nec iaculis est fermentum vitae. Maecenas sodales, nunc eget lacinia volutpat, nibh mi lobortis leo, id varius urna tellus
-                    ut magna. Sed porttitor nunc et luctus efficitur. Nam sit amet congue nulla. Praesent dapibus erat arcu, at scelerisque arcu faucibus vel. Morbi blandit venenatis elit. Nam feugiat bibendum suscipit. Donec viverra eu lorem eu venenatis. Nullam posuere
-                    nulla id libero rutrum venenatis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam molestie ac velit vel blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                    egestas.</p>
+                    <p>Praesent eu arcu convallis, faucibus turpis eu, rutrum nisi. Integer rutrum ipsum at aliquam consequat. In dignissim lorem nibh, nec iaculis est fermentum vitae. Maecenas sodales, nunc eget lacinia volutpat, nibh mi lobortis leo, id varius urna tellus
+                        ut magna. Sed porttitor nunc et luctus efficitur. Nam sit amet congue nulla. Praesent dapibus erat arcu, at scelerisque arcu faucibus vel. Morbi blandit venenatis elit. Nam feugiat bibendum suscipit. Donec viverra eu lorem eu venenatis.</p>
+                </div>
+
+
+
             </div>
-
-
-
-
 
         </div >
 
